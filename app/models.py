84c716794fc144810690ac0a40a2ba65dcdea6a5
ZhengyuanXiang <xiangsocool@gmail.com>
2017-09-10 17:58:04 +0800

    login

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
 from . import db
 from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
 
 class User(UserMixin, db.Model):
     __tablename__ = 'users'
@@ -7,3 +8,18 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(64), unique=True, index=True)
     username = db.Column(db.String(64), unique=True, index=True)
     password_hash = db.Column(db.String(128))
+
+    def __init__(self, **kwargs):
+        super(User, self).__init__(**kwargs)
+
+    @property
+    def password(self):
+        raise AttributeError('password is not a readable attribute')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+        print(self.password_hash)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
