f6290bbf604e7aff789a9a34ba5bd102f756a35c
ZhengyuanXiang <xiangsocool@gmail.com>
2017-09-10 14:33:42 +0800

    project setup

---
+++ app/models.py
@@ -0,0 +1,9 @@
+from . import db
+from flask_login import UserMixin
+
+class User(UserMixin, db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(64), unique=True, index=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    password_hash = db.Column(db.String(128))
