b3aa62b9d34395b07b8c165cd311965fba35e4f8
ZhengyuanXiang <xiangsocool@gmail.com>
2017-09-25 23:10:45 +0800

    websocket client

--- app/device_mng/device_mng.py
+++ app/device_mng/device_mng.py
@@ -1,9 +1,17 @@
 from . import socketio
+from flask_socketio import emit
 
 @socketio.on('connect')
 def handle_message():
     print('connect')
 
-@socketio.on('my event')
-def my_event():
-    print('my event resv')
+@socketio.on('login')
+def my_event(data):
+    print(data)
+    from ..models import User
+    user = User.query.filter_by(email=data['email']).first()
+    if user is not None and user.verify_password(data['password']):
+        print('%s login' % data['email'])
+        emit("login", "OK")
+        return
+    emit("login", "NOT OK")
--- tests/test_socketio.py
+++ tests/test_socketio.py
@@ -4,6 +4,12 @@ import logging
 logging.getLogger('socketIO-client').setLevel(logging.DEBUG)
 logging.basicConfig()
 
+def login_ret(data):
+    print(data)
+
 if __name__ == '__main__':
     socketIO = SocketIO('localhost', 5000)
     print('test_socketio')
+    socketIO.on('login', login_ret)
+    socketIO.emit('login', {'email':'xiangsocool@gmail.com', 'password':'123'})
+    socketIO.wait()
